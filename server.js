989dcf274749fdb96e28bcc6ad1f4ff2f58e76c3
SylviaRemington <sylviaremington@gmail.com>
2025-06-21 21:59:17 -0700

    notes regarding install packages - double checking with Tristan

--- server.js
+++ server.js
@@ -47,3 +47,21 @@ app.use('/auth', authController);
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
+
+
+/*
+NOTES FROM TERMINAL REGARDING npm i & the files installing / JUST DOUBLE CHECKING IF THEY ARE OK TO USE FROM TRISTAN:
+npm i
+npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+npm warn deprecated npmlog@5.0.1: This package is no longer supported.
+npm warn deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+npm warn deprecated are-we-there-yet@2.0.0: This package is no longer supported.
+npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm warn deprecated gauge@3.0.2: This package is no longer supported.
+
+added 173 packages, and audited 174 packages in 7s
+
+21 packages are looking for funding
+  run `npm fund` for details
+*/
